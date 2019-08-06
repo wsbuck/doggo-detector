954a0cdcb222019a4915847be57dc61bed25b10c
William Buck <wsbuckiii@gmail.com>
2019-08-06 15:10:19 -0700

    updated infoButton Dialog text colors

--- src/components/InfoButton.js
+++ src/components/InfoButton.js
@@ -4,12 +4,22 @@ import { DialogTitle, DialogActions } from '@material-ui/core';
 import { DialogContent, DialogContentText } from '@material-ui/core';
 import { Dialog, Button } from '@material-ui/core';
 import { IconButton } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Info from '@material-ui/icons/Info';
 
+const useStyles = makeStyles(theme => ({
+  bodyText: {
+    color: 'white',
+    '& a': {
+      color: '#FFAB91',
+    },
+  },
+}));
+
 export default function InfoButton() {
   const [open, setOpen] = React.useState(false);
-  // const classes = useStyles();
+  const classes = useStyles();
 
   function handleClickOpen() {
     setOpen(true);
@@ -35,7 +45,9 @@ export default function InfoButton() {
       >
         <DialogTitle id='alert-dialog-title'>Hello!</DialogTitle>
         <DialogContent>
-          <DialogContentText id='alert-dialog-description'>
+          <DialogContentText 
+            id='alert-dialog-description' 
+            className={classes.bodyText}>
             This is made with tensorflow.js and React.
             The model is mobilenet retrained only on the dogs of ImageNet.
             The code for this project can be found <a href="https://github.com/wsbuck/doge-detector">here</a> and the tensorflow model is <a href="https://github.com/wsbuck/DogeNetTFModel">here</a>.
